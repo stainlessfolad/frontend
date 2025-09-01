194ef2e598d0e6dbf5255d08f2a43f299dec872c
Ali-Hassanii <Ali-A.M.Hassani@outlook.com>
2025-09-01 20:34:00 +0330

    feat: improve UI

--- src/page/home/Header.tsx
+++ src/page/home/Header.tsx
@@ -1,134 +1,110 @@
-import { Box, Button, Stack, Typography, useTheme } from "@mui/material";
+import {
+  Box,
+  Button,
+  lighten,
+  Stack,
+  Typography,
+  useTheme,
+} from "@mui/material";
 
 function Header() {
   return (
     <Box
       component="header"
-      color="primary"
       sx={{
         backgroundColor: "secondary.dark",
         color: "secondary.contrastText",
         display: "flex",
-        flexDirection: "row",
+        flexDirection: { xs: "column", md: "row" },
         justifyContent: "space-evenly",
         paddingBottom: 12,
         px: 8,
       }}
     >
+      {/* Content Side */}
+      <Stack width={{ xs: "100%", md: "40%" }} alignItems="flex-end">
+        {/* Tubelight Effect */}
+        <Stack
+          direction="row-reverse"
+          spacing="-1px"
+          sx={{
+            width: "100%",
+            height: "100px",
+            top: 0,
+            marginBottom: 2,
+            position: "relative",
+            WebkitMask: `linear-gradient(rgba(0, 0, 0, 0.4), transparent)`,
+          }}
+        >
+          <Tubelight />
+          <Tubelight reverse />
+        </Stack>
+
+        {/* Header Text Content */}
+        <Stack component="div">
+          <Typography
+            variant="h2"
+            component="h1"
+            fontWeight="bold"
+            textAlign="center"
+            gutterBottom
+            sx={{ fontSize: { xs: "2.5rem", md: "3.75rem" } }}
+          >
+            گروه بازرگانی استیل
+          </Typography>
+          <Typography
+            variant="body1"
+            sx={{
+              mt: 2,
+              fontSize: { xs: "1rem", md: "1.25rem" },
+              color: "grey.400",
+            }}
+          >
+            تامین کننده مستقیم انواع ورق استیل به همراه گواهی تیسکو با هدف فروش
+            بدون واسطه و ارسال سریع به تمام نقاط ایران
+          </Typography>
+        </Stack>
+
+        <Button
+          variant="contained"
+          size="large"
+          href="/contact-us"
+          sx={{ mt: 4 }}
+        >
+          اطلاعات بیشتر
+        </Button>
+      </Stack>
+
+      {/* Picture Side */}
       <Box
-        component="div"
-        width="40%"
+        component="img"
+        src="/public/images/header_rolled_steel_and_sheets.webp"
+        alt="Rolled Steel and Sheets"
         sx={{
-          display: "flex",
-          flexDirection: "column",
+          objectFit: "cover",
+          minHeight: { xs: "200px" },
+          width: { xs: "100%", md: "40%" },
+          mt: 12,
         }}
-      >
-        <TubeLight height="100px" color="#fff" />
-        <HeroContent />
-      </Box>
+      />
     </Box>
   );
 }
 export default Header;
 
-function HeroContent({ width = "100%" }: { width?: string }) {
+function Tubelight({ reverse = false }: { reverse?: boolean }) {
+  const theme = useTheme();
   return (
     <Box
       sx={{
-        width: width,
-        left: 0,
+        flexGrow: 1,
+        transform: reverse ? "scaleX(-1)" : undefined,
+        background: `conic-gradient(
+              from 90deg at 60px 0%,
+              ${lighten(theme.palette.primary.main, 0.85)} 105deg,
+              transparent 125deg
+            )`,
       }}
-    >
-      <Typography
-        variant="h2"
-        component="h1"
-        fontWeight="bold"
-        gutterBottom
-        sx={{ fontSize: { xs: "2.5rem", md: "3.75rem" } }}
-      >
-        فروش ورق استیل
-      </Typography>
-      <Typography
-        variant="body1"
-        sx={{
-          mt: 2,
-          fontSize: { xs: "1rem", md: "1.25rem" },
-          color: "grey.400",
-        }}
-      >
-        واردات مستقیم و بدون واسطه انواع استیل با کیفیت عالی، کمترین قیمت و
-        ارسال سریع به تمام نقاط ایران
-      </Typography>
-      <Box sx={{ mt: 4, display: "flex", justifyContent: "flex-end", gap: 2 }}>
-        <Button
-          variant="contained"
-          color="primary"
-          size="large"
-          // TODO: Contact us section or page
-          href="#contact-us"
-          sx={{ textTransform: "none" }}
-        >
-          تماس با ما
-        </Button>
-        {/* <Button
-          variant="outlined"
-          color="secondary"
-          size="large"
-          href="#contact"
-          sx={{ textTransform: "none" }}
-        >
-          Learn More
-        </Button> */}
-      </Box>
-    </Box>
-  );
-}
-
-function TubeLight({
-  width = "100%",
-  height = "10px",
-  color,
-}: {
-  width?: string;
-  height?: string;
-  color?: string;
-}) {
-  const theme = useTheme();
-
-  return (
-    <Stack
-      direction="row-reverse"
-      spacing="-1px"
-      sx={{
-        width,
-        height,
-        top: 0,
-        marginBottom: 2,
-        position: "relative",
-        WebkitMask: `linear-gradient(rgba(0, 0, 0, 0.4), transparent)`,
-      }}
-    >
-      <Box
-        sx={{
-          flexGrow: 1,
-          background: `conic-gradient(
-            from 90deg at 60px 0%,
-            ${color ?? theme.palette.primary.main} 105deg,
-            transparent 125deg
-          )`,
-        }}
-      ></Box>
-      <Box
-        sx={{
-          flexGrow: 1,
-          background: `conic-gradient(
-            from 90deg at 60px 0%,
-            ${color ?? theme.palette.primary.main} 105deg,
-            transparent 125deg
-          )`,
-          transform: "scaleX(-1)",
-        }}
-      ></Box>
-    </Stack>
+    />
   );
 }
